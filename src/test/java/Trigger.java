6490117802af5b3eab1a475700811e2c29ec1398
akif <akif_karatas@oslerdiagnostics.com>
2022-10-28 16:31:47 +0300

    app folder added

--- src/test/java/Trigger.java
+++ src/test/java/Trigger.java
@@ -17,7 +17,7 @@ public class Trigger {
         String sourceFilePath = "";
 
         String sourceFolderPath = "C:/Users/akifm/Downloads/";
-        String targetFolderPath = "C:/Users/akifm/Documents/";
+        String targetFolderPath = "C:/Users/akifm/.jenkins/workspace/Test_Trigger_Build_Job_Demo/target/app/";
 
         //Check the source folder if it has the required file name
         File folder = new File(sourceFolderPath);
